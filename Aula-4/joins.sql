d2d025a117ec0d0e3676f8de083ec7b04b63d945
William Bruno <williambrunosalesdepaulalima@gmail.com>
2022-05-19 18:05:33 -0300

    joins

---
+++ Aula-4/joins.sql
@@ -0,0 +1,23 @@
+/*
+Possibilidade de unir tabelas a partir de campos em comum
+
+INNER JOIN:
+
+Seleciona apenas os registros com campos selecionados em comum
+
+LEFT JOIN:
+
+Seleciona todos os registros da tabela da esquerda e apenas
+os registros que tiverem correspondência da tabela da direita
+
+RIGHT JOIN:
+
+Seleciona todos os registros da tabela da direita e apenas os
+registros que tiverem campos correspondentes na tabela da
+esquerda
+
+CROSS JOIN:
+
+Retorna o produto cartesiano entre todos os registros possíveis
+das duas tabelas
+*/
