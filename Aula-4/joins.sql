fbc95aaf01fc7afcf1d897a16ebbf83f70376ab9
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2022-05-22 22:59:44 -0300

    adding joins content

--- Aula-4/joins.sql
+++ Aula-4/joins.sql
@@ -8,16 +8,54 @@ Seleciona apenas os registros com campos selecionados em comum
 LEFT JOIN:
 
 Seleciona todos os registros da tabela da esquerda e apenas
-os registros que tiverem correspondência da tabela da direita
+os registros que tiverem correspondência na tabela da direita.
+
+Os dados que não possuírem correspondência de alguma das tabelas
+virão como NULL.
 
 RIGHT JOIN:
 
 Seleciona todos os registros da tabela da direita e apenas os
 registros que tiverem campos correspondentes na tabela da
-esquerda
+esquerda.
+
+Os dados que não possuírem correspondência de alguma das tabelas
+virão como NULL.
+
+FULL JOIN
+
+O FULL JOIN retorna uma união entre as tabelas. Os dados que não
+possuírem correspondência ficarão com valor NULL.
 
 CROSS JOIN:
 
 Retorna o produto cartesiano entre todos os registros possíveis
-das duas tabelas
+das duas tabelas.
+
+Diferentemente dos outros JOINs, este não necessita de uma sentença
+que afirma que deve ser aplicado um CROSS JOIN, basta apenas uma
+vírgula separando as duas tabelas logo após o "FROM".
 */
+
+-- INNER JOIN
+SELECT * FROM tabela_de_vendedores A
+INNER JOIN notas_fiscais B
+ON A.matricula = B.matricula;
+
+/*
+Perceba que o bloco de código:
+
+FROM tabela_de_vendedores A
+INNER JOIN notas_fiscais B
+ON A.matricula = B.matricula;
+
+gera uma nova tabela. Desta forma, podemos
+utilizar isto para formar comandos SQL
+mais avançados utilizando JOINs e GROUP BY
+*/
+
+SELECT A.MATRICULA, A.NOME, COUNT(*) AS CONTAGEM_NOTAS
+FROM tabela_de_vendedores A
+INNER JOIN notas_fiscais B
+ON A.matricula = B.matricula
+GROUP BY A.MATRICULA;
