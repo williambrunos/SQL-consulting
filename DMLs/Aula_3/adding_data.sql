b08e133f03c006a766b1675bbc8da653c708f536
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2022-11-19 10:03:57 -0300

    feat: adding insert into class

---
+++ DMLs/Aula_3/adding_data.sql
@@ -0,0 +1,8 @@
+insert into <table_name> (columns)
+values (values);
+
+insert into produtos (codigo, descritor, sabor, tamanho, embalagem, preco_lista)
+values ('100', 'Light 350ml - Melância', 'Melância', '350ml', 'Lata', 4.56);
+
+-- Note: you can omiss the column names if the values order
+-- respects the column order of the table
