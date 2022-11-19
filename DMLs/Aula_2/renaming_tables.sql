6762af9862fd8867cd3df1b992d67d4d0d188a68
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2022-11-19 09:21:18 -0300

    feat: adding alter table rename class

---
+++ DMLs/Aula_2/renaming_tables.sql
@@ -0,0 +1,1 @@
+alter table <old_name> rename <new_name>;
--- DMLs/Aula_2/table_creation_FK.sql
+++ DMLs/Aula_2/table_creation_FK.sql
@@ -16,3 +16,19 @@ foreign key (cpf) references clientes (cpf);
 -- Adding "matricula" of vendedores as foreign key of vendas
 alter table vendas add constraint FK_vendedores
 foreign key (matricula) references vendedores (matricula);
+
+-- Another example
+
+create table itens_notas (
+	numero varchar(5) not null,
+    codigo varchar(10) not null,
+    quantidade int,
+    preco double,
+    primary key (numero, codigo)
+);
+
+alter table produtos add constraint FK_itens_notas
+foreign key (codigo) references itens_notas (codigo);
+
+alter table vendas add constraint FK_vendas
+foreign key (numero) references itens_notas (numero);
