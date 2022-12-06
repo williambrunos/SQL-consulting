efff65039dd2a88693a9cdc966f2ecf2284602a1
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2022-12-05 22:32:15 -0300

    feat: adding multiple data from another database

--- DMLs/Aula_3/adding_data.sql
+++ DMLs/Aula_3/adding_data.sql
@@ -6,3 +6,16 @@ values ('100', 'Light 350ml - Melância', 'Melância', '350ml', 'Lata', 4.56);
 
 -- Note: you can omiss the column names if the values order
 -- respects the column order of the table
+
+-- Inserting data from another database 
+
+insert into produtos (codigo, descritor, embalagem, tamanho, sabor, preco_lista) 
+select 
+	CODIGO_DO_PRODUTO as codigo,
+    NOME_DO_PRODUTO as descritor,
+    EMBALAGEM as embalagem, 
+    TAMANHO as tamanho,
+    SABOR as sabor,
+    PRECO_DE_LISTA as preco_lista
+from sucos_vendas.tabela_de_produtos
+where CODIGO_DO_PRODUTO not in (select codigo from vendas_sucos.produtos);
